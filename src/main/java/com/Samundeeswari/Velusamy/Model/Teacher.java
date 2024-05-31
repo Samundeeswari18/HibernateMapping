e1aa0c1428a704c2bb05b029071a88cd1c2630fe
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-05-30 21:40:06 -0400

    Hibernate ManyToMany Mapping

--- src/main/java/com/Samundeeswari/Velusamy/Model/Teacher.java
+++ src/main/java/com/Samundeeswari/Velusamy/Model/Teacher.java
@@ -1,10 +1,9 @@
 package com.Samundeeswari.Velusamy.Model;
-
-import com.Samundeeswari.Velusamy.Model.*;
 import jakarta.persistence.*;
-
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Set;
+
 @Entity
 @Table
 public class Teacher implements Serializable {
@@ -17,27 +16,46 @@ public class Teacher implements Serializable {
     private String teacherName;
 
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Address address;
+    // @OneToOne(cascade = CascadeType.ALL)
+    // private Address address;
 
-    public Address getAddress() {
-        return address;
+    @ManyToMany(targetEntity = Cohort.class)
+    private Set<Cohort> cohort;
+
+    public Set<Cohort> getCohort() {
+        return cohort;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    public void setCohort(Set<Cohort> cohort) {
+        this.cohort = cohort;
     }
 
+    //public Address getAddress() {
+    //     return address;
+    // }
 
-    public Teacher( String salary, String teacherName) {
+    // public void setAddress(Address address) {
+    //     this.address = address;
+    // }
+
+    public Teacher(String salary, String teacherName, Set<Cohort> cohort) {
+        this.salary = salary;
+        this.teacherName = teacherName;
+    }
+
+    public Teacher(String salary, String teacherName) {
         super();
         this.salary = salary;
         this.teacherName = teacherName;    }
     public Teacher() {}
+
     public Teacher(String salary, String teacherName, Department department) {
         this.salary = salary;
         this.teacherName = teacherName;
     }
+
+
+
     public int getTeacherId() {
         return teacherId;
     }
@@ -56,5 +74,4 @@ public class Teacher implements Serializable {
     public void setTeacherName(String teacherName) {
         this.teacherName = teacherName;    }
 }
-
 
